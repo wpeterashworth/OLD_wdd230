4f773e81c02d4e7f79ff5090036896c35647fed1
wpeterashworth <wingsaseagles12@gmail.com>
2024-04-26 23:53:47 -0600

    Began Work for Course Home Page

--- Practice/practice.js
+++ Practice/practice.js
@@ -1,4 +1,9 @@
 // Here i will change the background color with an event listener and basic dom strategies
 
-let colorButton = document.querySelector("#change-color")
-colorButton.addEventListener
+function changeColorFunction() {
+    const colorArray = ["cadetblue", "aqua", "aquamarine", "blue"];
+    document.body.style.backgroundColor = colorArray[Math.floor(Math.random() * colorArray.length)];
+}
+
+let colorButton = document.querySelector("#change-color");
+colorButton.addEventListener("click", changeColorFunction);
