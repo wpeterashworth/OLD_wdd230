6ae280375527762309aab8ff36b491793a62e2a2
wpeterashworth <wingsaseagles12@gmail.com>
2024-05-09 09:40:14 -0600

    updated home page (hometown map, waypoint finding, hamburger menu)

---
+++ scripts/hamburger.js
@@ -0,0 +1,6 @@
+let menuButton = document.querySelector("#menu-button");
+let navList = document.querySelector("nav ul");
+menuButton.addEventListener("click", () => {
+    menuButton.classList.toggle("open")
+    navList.classList.toggle("open")
+})
