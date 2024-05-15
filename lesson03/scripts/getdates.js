5fc612822dbebd544208f419b57026d1cebbb085
wpeterashworth <wingsaseagles12@gmail.com>
2024-05-15 13:56:01 -0600

    Started Chamber Site Home page

--- lesson03/scripts/getdates.js
+++
@@ -1,8 +0,0 @@
-const currentDate = new Date();
-const currentYear = currentDate.getFullYear();
-
-const year = document.querySelector("#date");
-year.innerHTML = `&copy${currentYear}`;
-
-let lastModifiedDate = document.querySelector("#lastModified");
-lastModifiedDate.innerHTML = new Date(document.lastModified);
